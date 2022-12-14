18945a015f8ef44ec4e47baea432aead85423e33
Iqbal Nugraha <iqbalnugraha347@gmail.com>
2022-12-14 22:13:05 +0700

    chore: Install dependency eslint, and set config for tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,44 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+      serif: ['Nunito Sans', 'serif'],
+    },
+    extend: {
+      fontSize: {
+        base: '16px',
+      },
+      screens: {
+        sm: '375px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {
+        primary: '#FF9315',
+        white: '#ffffff',
+        grey: {
+          dark: '#7E756B',
+          light: '#ADA7A0',
+        },
+        bg: {
+          dark: '#16212C',
+          light: '#FCFCFC',
+        },
+        red: {
+          DEFAULT: '#EF5950',
+        },
+        green: {
+          DEFAULT: '7ACB9A',
+        },
+        orange: {
+          light: '#FFF3E4',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
