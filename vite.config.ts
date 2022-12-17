82a83558748e8a175a989a1ca6c58b290178f24a
Iqbal Nugraha <iqbalnugraha347@gmail.com>
2022-12-17 08:41:56 +0700

    chore: setting vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
-import { default as path } from 'path';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
